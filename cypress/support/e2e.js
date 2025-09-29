6332fefae0ca0ffabdf8e0d2b68281f26d53289c
Mariel <eguigurensmariell@gmail.com>
2025-09-29 16:08:50 -0600

    Estructura + Allure report + schema (ajv para validarlo)

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,18 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import '@shelex/cypress-allure-plugin' ;
+import Ajv from "ajv";
+
+import addFormats from "ajv-formats";
+
+Cypress.Commands.add("validateSchema", (schema, data) => {
+  const ajv = new Ajv({ allErrors: true, strict: false });
+  addFormats(ajv);
+  const validate = ajv.compile(schema);
+  const valid = validate(data);
+  if (!valid) {
+    const pretty = JSON.stringify(validate.errors, null, 2);
+    throw new Error("Error en la validacion del Schema:\n" + pretty);
+  }
+});
